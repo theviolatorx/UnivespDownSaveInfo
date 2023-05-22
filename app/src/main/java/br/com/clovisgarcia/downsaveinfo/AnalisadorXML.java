71dc041128e636eeb2e30773a035d3db2b063aa6
Clovis Garcia <theviolatorx@gmail.com>
2023-05-22 06:13:46 -0300

    add class AnalisandoXML.java

--- app/src/main/java/br/com/clovisgarcia/downsaveinfo/AnalisadorXML.java
+++ app/src/main/java/br/com/clovisgarcia/downsaveinfo/AnalisadorXML.java
@@ -1,5 +1,12 @@
 package br.com.clovisgarcia.downsaveinfo;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.StringReader;
+
 public class AnalisadorXML {
     private String xmlDados;
 
@@ -14,6 +21,51 @@ public class AnalisadorXML {
     }
 
     public void process() {
-        
+
+        boolean campoValido = false;
+        boolean busca = true;
+
+        try {
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+            factory.setNamespaceAware(true);
+
+            XmlPullParser xpp = factory.newPullParser();
+
+            xpp.setInput(new StringReader(xmlDados));
+
+            int tipoDeEvento;
+
+            while (busca){
+
+                tipoDeEvento = xpp.next();
+                String tagName = xpp.getName();
+
+                switch (tipoDeEvento){
+
+                    case XmlPullParser.START_TAG:
+
+                        if (tagName.equalsIgnoreCase("body")) {
+                            campoValido = true;
+                        }
+
+                        break;
+
+                    case XmlPullParser.TEXT:
+
+                        if (campoValido){
+
+                            conteudo = xpp.getText();
+
+                            busca  = false;
+                        }
+                        break;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
